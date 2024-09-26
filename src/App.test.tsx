c8a565c4dd6f29769fd8ae7651d3c7b10d847ca9
GersonEC <gersonenriquez94@gmail.com>
2024-09-26 18:30:30 +0200

    install test libraries

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import { render } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import App from './App';
+
+test('should render properly', () => {
+  render(<App />);
+  expect(true).toBeTruthy();
+});
