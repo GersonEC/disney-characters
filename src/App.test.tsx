7d2369d4b84418cae46b544aea40a2f250361117
GersonEC <gersonenriquez94@gmail.com>
2024-10-01 14:11:57 +0200

    clean card and app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,7 @@
 /**
  * @vitest-environment jsdom
  */
-
-import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
-import { RouterProvider } from '@tanstack/react-router';
-import App from './App';
-import { router } from './utils/mocks';
 
 test('should render properly', () => {
   expect(true).toBeTruthy();
--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -2,37 +2,25 @@
  * @vitest-environment jsdom
  */
 
-import { cleanup, render, screen } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
-import { afterEach, expect, test, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { test } from 'vitest';
 import { Card } from './Card';
 import { characterMock } from '../../utils/mocks';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+/*import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import {
   RouterProvider,
   createRootRoute,
   createRouter,
 } from '@tanstack/react-router';
-import React from 'react';
 
 const queryClient = new QueryClient();
 
 export const rootRoute = createRootRoute({
-  component: () => (
-    <React.Fragment>
-      <Card
-        films={characterMock.films}
-        id={characterMock._id}
-        imgUrl={characterMock.imageUrl ?? ''}
-        name={characterMock.name}
-      />
-    </React.Fragment>
-  ),
+  component: () => <React.Fragment></React.Fragment>,
 });
 
-export const routeTree = rootRoute.addChildren([]);
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree: rootRoute });
 
 const renderWithRouter = (component: React.ReactElement) => {
   return render(
@@ -41,9 +29,9 @@ const renderWithRouter = (component: React.ReactElement) => {
     </QueryClientProvider>
   );
 };
-
+*/
 test('should render properly', () => {
-  renderWithRouter(
+  render(
     <Card
       films={characterMock.films}
       id={characterMock._id}
